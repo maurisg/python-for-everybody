5412c4659baf4e83985b9407148bead40200d9a4
Mauricio Salas <msalas2199@hotmail.com>
2020-09-16 14:00:20 -0500

    Exercise 4
    Assume that we execute the following assignment statements.

---
+++ Chapter 2/exercise_04.py
@@ -0,0 +1,12 @@
+width = input("width: ")
+height = input("height: ")
+
+first_expression = float(width)//2
+second_expression = float(width)/2.0
+third_expression = float(height)/3
+fourth_expression = 1 + 2 * 5
+
+print(first_expression)
+print(second_expression)
+print(third_expression)
+print(fourth_expression)
