b2b77edcad04cffaf8e1fc5993062dac11ad319f
Mauricio Salas <msalas2199@hotmail.com>
2020-09-16 21:01:46 -0500

    Exercise 7
    Rewrite the grade program from the previous chapter using a function called computegrade that takes a score as its parameter and returns a grade as a string.

---
+++ Chapter 4/exercise_07.py
@@ -0,0 +1,28 @@
+def computegrade():
+    grade = input("Enter grade [0.0 - 1.0]: ")
+    try:
+        grade = float(grade)
+        if 0 <= grade <= 1.0:
+            if grade >= 0.9:
+                print("Your grade: A")
+
+            elif 0.8 <= grade < 0.9:
+                print("Your grade: B")
+
+            elif 0.7 <= grade < 0.8:
+                print("Your grade: C")
+
+            elif 0.6 <= grade < 0.7:
+                print("Your grade: D")
+
+            else:
+                print("Your grade: F")
+
+        else:
+            print("Please enter a valid grade.")
+
+    except:
+        print("Please enter a valid grade.")
+
+
+computegrade()
