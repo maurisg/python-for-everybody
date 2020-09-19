09fb837988d1afcd334290cee1eda407adde2876
Mauricio Salas <msalas2199@hotmail.com>
2020-09-18 23:02:59 -0500

    Exercise 1
    Write a while loop that starts at the last character in the string and works its way backwards to the first character in the string, printing each letter on a separate line, except backwards.

--- Chapter 6/exercise_01.py
+++ Chapter 6/exercise_01.py
@@ -1,9 +1,9 @@
 index = 0
-variable = 0
+char = 0
 entry = input("Please write a word or sentence: ")
 
 while len(entry) > index:
-    letter = entry[variable - 1]
+    letter = entry[char - 1]
     print(letter)
-    variable = variable - 1
+    char = char - 1
     index = index + 1
