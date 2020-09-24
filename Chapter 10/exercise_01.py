8faf747072327e6560b2f9b97afe68168a1e9ee4
Mauricio Salas <msalas2199@hotmail.com>
2020-09-23 22:42:59 -0500

    Exercise 1
    Revise a previous program as follows: Read and parse the “From” lines and pull out the addresses from the line. Count the number of messages from each person using a dictionary.
    
    After all the data has been read, print the person with the most commits by creating a list of (count, email) tuples from the dictionary. Then sort the list in reverse order and print out the person who has the most
    commits.

--- Chapter 10/exercise_01.py
+++ Chapter 10/exercise_01.py
@@ -1,1 +1,34 @@
-# This program is made to not lose the streak, tomorrow I will finish the Tuple's chapter and start the exercises
+my_dictionary = {}                                # Creates an empty dictionary
+my_list = []                                      # Creates an empty list
+  
+file = input("Enter file name: ")
+maximum = 0
+maximum_address = ""
+
+try:
+    file = open(file)
+
+except FileNotFoundError:
+    print("File cannot be opened:", file)
+    exit()
+
+for line in file.readlines():
+    words = line.split()
+    if len(words) < 3 or words[0] != "From":
+        continue
+
+    else:
+        if words[1] not in my_dictionary:
+            my_dictionary[words[1]] = 1
+
+        else:
+            my_dictionary[words[1]] += 1
+
+for email, count in my_dictionary.items():        # Makes a list of tuples
+    my_tuple = (count, email)
+    my_list.append(my_tuple)
+
+my_list = sorted(my_list, reverse=True)           # The list is sorted from largest to smallest
+
+for count, email in my_list[:1]:                  # Prints the first one (largest)
+    print(email, count)
