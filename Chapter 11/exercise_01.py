9177b129d67fb87aab2cda4971243ba16db7cee7
Mauricio Salas <msalas2199@hotmail.com>
2020-09-26 12:25:36 -0600

    Exercise 1
    Write a simple program to simulate the operation of the grep command on Unix. Ask the user to enter a regular expression and count the number of lines that matched the regular expression.

--- Chapter 11/exercise_01.py
+++ Chapter 11/exercise_01.py
@@ -1,1 +1,13 @@
-# Reddit
+import re
+
+count = 0
+file = open("mbox-short.txt")
+expression = input("Enter a regular expression: ")
+
+for line in file:
+    line = line.rstrip()
+    x = re.findall(expression, line)
+    if len(x) > 0:
+        count += 1
+
+print("\'mbox-short.txt\' had", count, "lines that matched:", expression)
