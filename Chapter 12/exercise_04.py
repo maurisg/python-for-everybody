63c2a8d2d60229acee7737fd7b6578167c4c4cfe
Mauricio Salas <msalas2199@hotmail.com>
2020-09-28 13:53:19 -0600

    Exercise 4
    Change the urllinks.py program to extract and count paragraph (p) tags from the retrieved HTML document and display the count of the paragraphs as the output of your program. Do not display the paragraph text, only count them. Test your program on several small web pages as well as some larger web pages.

---
+++ Chapter 12/exercise_04.py
@@ -0,0 +1,18 @@
+import urllib.request
+import urllib.parse
+import urllib.error
+import ssl
+from bs4 import BeautifulSoup
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter URL: ')
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+# Retrieve all the paragraphs
+paragraphs = soup('p')
+print(len(paragraphs))
